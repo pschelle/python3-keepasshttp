b40621fad085e1234e3fe16295dbe344bf7eef59
Markus Freitag <fmarkus@mailbox.org>
2017-09-03 11:06:21 +0200

    Add docstrings and change methods names to avoid mixedCase.

--- keepasshttp/password.py
+++ keepasshttp/password.py
@@ -1,22 +1,36 @@
+"""Submodule of keepasshttp, providing a Password wrapper class."""
+
+
 class Password(object):
-    """A simple object that stores a password but prints '***'
+    """A simple object that stores a password but prints '***'.
 
     This helps prevent accidentally printing a password to the terminal.
     """
+
     def __init__(self, password):
+        """Initialize a new Password object."""
         self.value = password
 
     def __str__(self):
+        """Conversation method from object to string."""
         return '*****'
 
     def __repr__(self):
+        """Create a object represensation string."""
         return '{}(*****)'.format(self.__class__.__name__)
 
 
-def _isPassword(key):
+def _is_password(key):
+    """Check whether a key is 'password'.
+
+    Args:
+        key (str): dictionary key
+    Returns
+        bool: true if key is 'password'
+    """
     return key.lower() == 'password'
 
 
 def replace(mapping):
-    """Replaces the values for keys that look like passwords"""
-    return {k: Password(v) if _isPassword(k) else v for k, v in mapping.items()}
+    """Replace the values for keys that look like passwords."""
+    return {k: Password(v) if _is_password(k) else v for k, v in list(mapping.items())}
