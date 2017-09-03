9f04118e488f029e97e6bc0e60e6ca9a2c48bc90
Markus Freitag <fmarkus@mailbox.org>
2017-09-03 11:14:35 +0200

    Add docstrings and change methods names to avoid mixedCase.

--- keepasshttp/session.py
+++ keepasshttp/session.py
@@ -1,22 +1,27 @@
+"""Submodule of keepasshttp, providing session handling."""
 import logging
 import os
 import yaml
 
 import xdg.BaseDirectory
 
-from keepasshttp import protocol
+from . import protocol
 
 
 logger = logging.getLogger(__name__)
 
 
 class Session(object):
+    """Wrapper class to handle communication functions."""
+
     def __init__(self, key, id_):
+        """Initialize a new Session object."""
         self.key = key
         self.id_ = id_
 
     @classmethod
-    def start(cls, app_name):
+    def start(cls, appname):
+        """Start a new communication session."""
         config_dir = xdg.BaseDirectory.save_config_path(app_name)
         config_path = os.path.join(config_dir, 'keepasshttp.yml')
         if os.path.exists(config_path):
@@ -24,20 +29,34 @@ class Session(object):
                 config = yaml.safe_load(fin)
             id_ = config['id']
             key = config['key']
-            if not protocol.testAssociate(id_, key):
+            if not protocol.test_associate(id_, key):
                 logger.warning("Previous association failed. Loading new association")
-                key, id_ = getAndSaveNewAssociation(config_path)
+                key, id_ = get_and_save_new_association(config_path)
         else:
             logger.info("No previous association. Loading new association")
-            key, id_ = getAndSaveNewAssociation(config_path)
+            key, id_ = get_and_save_new_association(config_path)
         return cls(key, id_)
 
-    def getLogins(self, url):
-        return protocol.getLogins(url, self.id_, self.key)
+    def get_logins(self, url):
+        """Get all entries which match the given url.
+
+        Args:
+            url (str): url to look for
+        Returns:
+            list: matching entries
+        """
+        return protocol.get_logins(url, self.id_, self.key)
 
 
-def getAndSaveNewAssociation(config_path):
+def get_and_save_new_association(config_path):
+    """Associate a new key and store it.
+
+    Args:
+        config_path (str): filepath to the configfile
+    Returns:
+        str, str: new pair of key and its id
+    """
     key, id_ = protocol.associate()
     with open(config_path, 'w') as fout:
-        fout.write(yaml.safe_dump({'key': key, 'id': id_}, default_flow_style=False))
+        fout.write(json.dumps({'key': key.decode('utf-8'), 'id': id_}))
     return key, id_
