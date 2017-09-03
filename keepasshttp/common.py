3c4cb7fc98579171d2e814f30cd528acb1b8d2b6
Markus Freitag <fmarkus@mailbox.org>
2017-09-03 11:07:03 +0200

    Add docstrings and change methods names to avoid mixedCase.

--- keepasshttp/common.py
+++ keepasshttp/common.py
@@ -1,4 +1,10 @@
+"""Submodule of keepasshttp, providing custom exceptions."""
+
+
 class RequestFailed(Exception):
+    """Custom Exception, representing a failed request."""
+
     def __init__(self, message, response):
+        """Initialize a new RequestFailed exception."""
         Exception.__init__(self, message)
         self.response = response
